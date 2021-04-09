2a89d522eab13f4c6c1b8207b148144b257552b6
Eduardo <edufcarvalho@gmail.com>
2021-04-09 20:52:04 -0300

    configurações e index dos slides

--- js/slide.js
+++ js/slide.js
@@ -42,7 +42,7 @@ export default class Slide {
 
   // Reponsável por encerrar a atividade ao desclicar
   onEnd(event) {
-    const moveType = event === "mouseup" ? "mousemove" : "touchmove";
+    const moveType = event.type === "mouseup" ? "mousemove" : "touchmove";
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
   }
@@ -61,9 +61,39 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
+  slidePosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  // Slides Config
+  slidesConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePosition(element);
+      return { position, element };
+    });
+  }
+
+  slidesIndexNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1,
+    };
+  }
+
+  changeSlide(index) {
+    const activePosition = this.slideArray[index];
+    this.moveSlide(activePosition.position);
+    this.slidesIndexNav(index);
+    this.dist.finalPosition = activePosition.position;
+  }
+
   init() {
     this.bindEvents();
     this.addSlideEvents();
+    this.slidesConfig();
     return this;
   }
 }
