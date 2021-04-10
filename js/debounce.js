e404ff7297d1fa1458395e237873726cea0ae93a
Eduardo <edufcarvalho@gmail.com>
2021-04-10 19:53:19 -0300

    Ajuste das configurações de resize e active class adicionadas

--- js/debounce.js
+++ js/debounce.js
@@ -8,3 +8,5 @@ export default function debounce(callback, delay) {
     }, delay);
   };
 }
+
+// A função debounce é utilizada para quando temos uma atividade que realiza um repetição de ações sem necessidade fazendo com que o sistema sobrecarregue desnecessáriamente, ele vai atuar fazendo com que o item aguarde o final da ação para ocorrer, assim acontecendo somente 1 vez, como no resize por exemplo.
